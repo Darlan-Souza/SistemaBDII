aa34f5f5ab1796e4a2cbfde34933fdfbf652fe9d
Guilherme Rocha <gleite98@gmail.com>
2019-01-10 12:21:40 -0200

    Colocando para abrir as listagens em nova aba

--- topo.php
+++ topo.php
@@ -13,13 +13,13 @@
                 </div>
                 <div class="menu">
                     <ul>
-                        <li><a href="listar.php?msg=aluno">LISTAR ALUNOS</a></li>
-                        <li><a href="listar.php?msg=professor">LISTAR PROFESSORES</a></li>
-                        <li><a href="listar.php?msg=curso">LISTAR CURSOS</a></li>
-                        <li><a href="listar.php?msg=mensalidade">LISTAR MENSALIDADES</a></li>
-                        <li><a href="listar.php?msg=despesa">LISTAR DESPESAS</a></li>
-                        <li><a href="listar.php?msg=matricula">LISTAR MATRÍCULAS</a></li>
-                        <li><a href="listar.php?msg=diretor">LISTAR DIRETORES</a></li>
+                        <li><a href="listar.php?msg=aluno" target="_blank">LISTAR ALUNOS</a></li>
+                        <li><a href="listar.php?msg=professor" target="_blank">LISTAR PROFESSORES</a></li>
+                        <li><a href="listar.php?msg=curso" target="_blank">LISTAR CURSOS</a></li>
+                        <li><a href="listar.php?msg=mensalidade" target="_blank">LISTAR MENSALIDADES</a></li>
+                        <li><a href="listar.php?msg=despesa" target="_blank">LISTAR DESPESAS</a></li>
+                        <li><a href="listar.php?msg=matricula" target="_blank">LISTAR MATRÍCULAS</a></li>
+                        <li><a href="listar.php?msg=diretor" target="_blank">LISTAR DIRETORES</a></li>
                     </ul>
                 </div>
             </div>
