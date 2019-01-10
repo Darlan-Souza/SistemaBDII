8f7ee03f4086591bb823aa4823cc2d69b50fa99c
Luiz Araujo <luiz.araujodtna@gmail.com>
2019-01-10 11:15:42 -0200

    Otimização dos códigos de listar

--- topo.php
+++ topo.php
@@ -13,13 +13,13 @@
                 </div>
                 <div class="menu">
                     <ul>
-                        <li><a href="listaAlunos.php" target="_blank">LISTAR ALUNOS</a></li>
-                        <li><a href="listaProfessores.php" target="_blank">LISTAR PROFESSORES</a></li>
-                        <li><a href="listaCursos.php" target="_blank">LISTAR CURSOS</a></li>
-                        <li><a href="listaMensalidades.php" target="_blank">LISTAR MENSALIDADES</a></li>
-                        <li><a href="listaDespesas.php" target="_blank">LISTAR DESPESAS</a></li>
-                        <li><a href="listaMatriculas.php" target="_blank">LISTAR MATRÍCULAS</a></li>
-                        <li><a href="listaDiretores.php" target="_blank">LISTAR DIRETORES</a></li>
+                        <li><a href="listar.php?msg=aluno">LISTAR ALUNOS</a></li>
+                        <li><a href="listar.php?msg=professor">LISTAR PROFESSORES</a></li>
+                        <li><a href="listar.php?msg=curso">LISTAR CURSOS</a></li>
+                        <li><a href="listar.php?msg=mensalidade">LISTAR MENSALIDADES</a></li>
+                        <li><a href="listar.php?msg=despesa">LISTAR DESPESAS</a></li>
+                        <li><a href="listar.php?msg=matricula">LISTAR MATRÍCULAS</a></li>
+                        <li><a href="listar.php?msg=diretor">LISTAR DIRETORES</a></li>
                     </ul>
                 </div>
             </div>
